ff82627cc82d8d1e88da391954b1b9d9447fb5a6
fewfew fewfwefew feffw <97164307+d5rkd5ark@users.noreply.github.com>
2022-01-16 23:10:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+	$('.header__burger').click(function () {
+		$('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+	});
+   
+   
+   $(function(){
+
+      $('.tiles__item').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 425,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: true
+            }
+          }
+        ]
+      });
+   });
+
+   
+});
+
