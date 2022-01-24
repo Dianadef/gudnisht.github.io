e9df7f1c1efc799a46f36753ae6c912d50a2d589
fewfew fewfwefew feffw <97164307+d5rkd5ark@users.noreply.github.com>
2022-01-24 10:51:58 +0300

    Update main_two.js

--- js/main_two.js
+++ js/main_two.js
@@ -1,8 +1,8 @@
 /*global $ */
 $(document).ready(function () {
    "use strict";
-	$('.header__burger').click(function () {
-		$('.header__burger, .header__menu').toggleClass('active');
+   $('.header__burger').click(function () {
+      $('.header__burger, .header__menu').toggleClass('active');
       $('body').toggleClass('lock');
-      });
+    });
 });
